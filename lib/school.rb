8329d979a664ee44919145d9e796d369a768aee1
Ammar Ali <funnyguy118@gmail.com>
2019-02-20 20:47:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+
+class School
+  
+  attr_accessor :name, :roster 
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end 
+  
+  def grade
+  end 
+  
+  def sort 
+  end 
+  
+end
